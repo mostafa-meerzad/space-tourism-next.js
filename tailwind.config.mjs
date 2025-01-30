d000e58a31c59d7a6174d55aff9133065ffb0539
mostafa <mostafameerzad@gmail.com>
2025-01-30 19:25:05 +0430

    Add background images to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import { crew, destinations, technology } from "./app/constants";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -14,10 +16,30 @@ export default {
         midGray: "#979797",
         white: "#FFFFFF",
       },
-      fontFamily:{
+      fontFamily: {
         barlow: ["var(--font-barlow)", "sans-serif"],
         bellefair: ["var(--font-bellefair)", "serif"],
-      }
+      },
+      backgroundImage: {
+        homeDesktop: "url(/images/home/background-home-desktop.jpg)",
+        homeTablet: "url(/images/home/background-home-tablet.jpg)",
+        homeMobile: "url(/images/home/background-home-mobile.jpg)",
+        destinationsDesktop:
+          "url(/images/destinations/background-destinations-desktop.jpg)",
+        destinationsTablet:
+          "url(/images/destinations/background-destinations-tablet.jpg)",
+        destinationsMobile:
+          "url(/images/destinations/background-destinations-mobile.jpg)",
+        technologyDesktop:
+          "url(/images/technology/background-technology-desktop.jpg)",
+        technologyTablet:
+          "url(/images/technology/background-technology-tablet.jpg)",
+        technologyMobile:
+          "url(/images/technology/background-technology-mobile.jpg)",
+        crewDesktop: "url(/images/crew/background-crew-desktop.jpg)",
+        crewTablet: "url(/images/crew/background-crew-tablet.jpg)",
+        crewMobile: "url(/images/crew/background-crew-mobile.jpg)",
+      },
     },
   },
   plugins: [],
