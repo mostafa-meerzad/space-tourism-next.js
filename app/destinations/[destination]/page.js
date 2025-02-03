8ce47fe491f30291628600866515a0ddb0630b49
mostafa <mostafameerzad@gmail.com>
2025-02-03 20:08:39 +0430

    Destination: Compete tablet view

--- app/destinations/[destination]/page.js
+++ app/destinations/[destination]/page.js
@@ -8,26 +8,26 @@ const DestinationPage = async ({ params }) => {
     destinations[destination];
 
   return (
-    <section className="flex flex-col gap-24 justify-center items-center pt-44 pb-10">
-      <div className=" w-52 h-52">
+    <section className="flex flex-col gap-24 md:gap-32 justify-center items-center pt-44 md:pt-48 pb-10">
+      <div className=" w-52 h-52 md:w-60 md:h-60">
         <Image src={images[destination]} alt={destination} />
       </div>
 
-      <div className="flex flex-col items-center px-8 text-white">
+      <div className="flex flex-col items-center px-8 md:px-24 text-white max-w-screen-sm md:max-w-screen-md">
         <h2 className="text-7xl font-barlows uppercase font-normal">{name}</h2>
         <p className="text-center font-barlow tracking-wide [word-spacing:2px] text-lightGray px-5">
           {description}
         </p>
-        <hr className="bg-darkGray opacity-30 w-full my-9 h-[1px]" />
+        <hr className="bg-darkGray opacity-20 w-full my-9 h-[1px]" />
 
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col items-center gap-2 text-lightGray uppercase ">
+        <div className="flex flex-col md:flex-row gap-8 md:gap-20 ">
+          <div className="flex flex-col items-center md:items-start gap-2 text-lightGray uppercase ">
             <span className=" text-md tracking-widest">avg. distance</span>{" "}
             <span className="text-3xl text-white font-normal tracking-wider font-sans">
               {distance}
             </span>
           </div>
-          <div className="flex flex-col items-center gap-2 text-lightGray uppercase ">
+          <div className="flex flex-col items-center md:items-start gap-2 text-lightGray uppercase ">
             <span className=" text-md tracking-widest">est. travel time</span>{" "}
             <span className="text-3xl text-white tracking-wider font-normal font-sans">
               {travel}
