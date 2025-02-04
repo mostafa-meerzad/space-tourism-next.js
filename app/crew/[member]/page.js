42e44370d68caceffc640b2918af2651ef8f585b
mostafa <mostafameerzad@gmail.com>
2025-02-04 21:59:45 +0430

    Crew: Complete desktop view

--- app/crew/[member]/page.js
+++ app/crew/[member]/page.js
@@ -7,24 +7,28 @@ const CrewPage = async ({ params }) => {
   const { name, images, role, bio } = crew[member];
 
   return (
-    <section className="grid justify-center items-center mt-48 mb-20 md:mb-0">
-      <div className=" flex flex-col items-center justify-center md:order-3">
+    <section className="grid justify-center items-center mt-48 mb-20 md:mb-0 lg:grid-flow-row lg:grid-cols-2 ">
+      <div className=" flex flex-col items-center justify-center md:order-3 lg:order-2">
         <Image
-          className="w-48 h-64 md:h-[25rem] md:w-[18rem] object-contain object-bottom"
+          className="w-48 h-64 md:h-[25rem] md:w-[18rem] object-contain object-bottom lg:h-[30rem] lg:w-[25rem]"
           src={images[member]}
           alt={name}
         />
         <hr className="w-4/5 h-0.5 bg-darkGray opacity-30 md:hidden" />
       </div>
 
-      <div className=" h-10 md:order-2  md:h-24"></div>
+      <div className=" h-10 md:order-2  md:h-24 lg:hidden"></div>
 
-      <div className="flex flex-col items-center justify-center gap-2 font-barlow md:order-1">
-        <h2 className="text-darkGray text-lg uppercase font-semibold tracking-tight ">
+      <div className="flex flex-col items-center justify-center gap-2 lg:gap-4 font-barlow md:order-1 lg:items-start -mt-4">
+        <h2 className="text-darkGray text-lg uppercase font-semibold tracking-tight lg:text-2xl lg:font-medium">
           {role}
         </h2>
-        <h3 className="text-2xl uppercase font-semibold text-white">{name}</h3>
-        <p className="text-lightGray text-center max-w-xl">{bio}</p>
+        <h3 className="text-2xl uppercase font-semibold text-white lg:text-5xl lg:font-medium ">
+          {name}
+        </h3>
+        <p className="text-lightGray text-center max-w-xl lg:text-start lg:max-w-sm">
+          {bio}
+        </p>
       </div>
     </section>
   );
--- app/crew/layout.js
+++ app/crew/layout.js
@@ -5,14 +5,14 @@ import Link from "next/link";
 const CrewLayout = ({ children }) => {
   return (
     <section className="grid grid-cols-2 grid-rows-2 px-10 lg:px-32 crew-bg-img bg-img-config">
-      <div className="grid grid-rows-3 grid-cols-1 gap-2 h-min col-start-1 col-end-3 row-start-1 row-end-4 text-white z-10 mt-32 ">
-        <h1 className="flex gap-2 text-xl font-medium font-barlow uppercase tracking-widest [word-spacing:5px] ">
+      <div className="grid grid-rows-3 grid-cols-1 gap-2 h-min col-start-1 col-end-3 row-start-1 row-end-4 text-white z-10 mt-32 lg:mt-40 ">
+        <h1 className="flex gap-2 text-xl font-medium font-barlow uppercase tracking-widest [word-spacing:5px] lg:row-start-1 lg:row-end-2">
           <span className="text-darkGray">02</span> <span>Meet your crew</span>
         </h1>
 
-        <div className="h-40 md:h-28"></div>
+        <div className="h-40 md:h-28 lg:hidden"></div>
 
-        <ul className="flex flex-row items-center content-center gap-4  h-max mx-auto">
+        <ul className="flex flex-row items-center content-center gap-4  h-max mx-auto lg:mx-0 lg:row-start-3 lg:mt-28">
           {Object.keys(crew).map((item) => (
             <li key={item}>
               <Link
@@ -25,7 +25,7 @@ const CrewLayout = ({ children }) => {
         </ul>
       </div>
 
-      <div className="col-start-1 col-end-3 row-start-1 row-end-4">
+      <div className="col-start-1 col-end-3 row-start-1 row-end-4 ">
         {children}
       </div>
     </section>
