5aefc6116e831f002567a05c2c639590a7e0055c
mostafa <mostafameerzad@gmail.com>
2025-02-08 18:06:47 +0430

    fix(destinations): Fix Layout component
    Fix and simplify the layout,  structure and styles

--- app/destinations/layout.js
+++ app/destinations/layout.js
@@ -6,36 +6,41 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const DestinationLayout = ({ children }) => {
-  const pathname = usePathname();
-
   return (
-    <section className="destination-bg-img bg-img-config grid grid-cols-2 px-10 lg:px-32 pb-10">
-      <div className="grid grid-cols-1 row-start-1 col-start-1 col-end-3  lg:grid-cols-2 lg:grid-rows-3 justify-center lg:pt-10 text-white  max-h-80 mt-32 lg:h-32">
-        <h1 className="flex justify-center md:justify-start gap-2 text-xl font-medium font-barlow uppercase tracking-widest [word-spacing:5px] lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-2 ">
-          <span className="text-darkGray font-bold ">01</span> Pick your
-          destination
-        </h1>
-
-        <div className=" h-64 md:h-80 lg:h-20 lg:col-start-2 lg:col-end-3 lg:row-start-2 lg:row-end-3 lg:hidden"></div>
-
-        <ul className=" flex gap-4 mx-auto justify-evenly lg:justify-start font-barlow uppercase tracking-widest h-9 w-4/5 max-w-80 lg:col-start-2 lg:col-end-3 lg:row-start-3 lg:row-end-4  lg:mx-0 ">
-          {Object.keys(destinations).map((item) => (
-            <li key={item} className="relative">
-              <Link href={`/destinations/${item}`}>{item}</Link>
-              <div
-                className={`absolute h-[.2rem] w-3/4 rounded-lg bg-lightGray top-3/4 lef-0  ${
-                  pathname === `/destinations/${item}` ? "block" : "hidden"
-                }`}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+    <section className="destination-bg-img bg-img-config pt-1">
+      <ul className="absolute top-[27rem] md:top-[31rem] lg:top-48 flex justify-evenly lg:justify-start gap-4 mx-auto left-0 right-0 h-9  max-w-80 lg:translate-x-0 lg:left-1/2 lg:right-auto">
+        {Object.keys(destinations).map((item) => (
+          <NavLink label={item} key={item} />
+        ))}
+      </ul>
 
       <div className=" col-start-1 col-end-3 row-start-1 mt-44 md:mt-52 lg:mt-64">
         {children}
       </div>
     </section>
+  );
+};
+
+const NavLink = ({ label }) => {
+  const pathname = usePathname();
+  const isActive = pathname === `/destinations/${label}`;
+  return (
+    <li
+      className={`relative font-barlow uppercase tracking-widest text-white  ${
+        isActive ? "hover:text-white" : "hover:text-gray-400"
+      } group`}
+    >
+      <Link href={`/destinations/${label}`}>
+        {label}
+        <div
+          className={`absolute h-[.2rem] w-3/4 rounded-lg  top-3/4 lef-0
+             bg-lightGray
+            ${
+              isActive ? "block group-hover:bg-lightGray" : "hidden"
+            } group-hover:block group-hover:bg-gray-500`}
+        />
+      </Link>
+    </li>
   );
 };
 
