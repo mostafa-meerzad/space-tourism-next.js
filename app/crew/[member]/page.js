1e24f72a3032f55c1dee7bead06cc8cb87b9cb68
mostafa <mostafameerzad@gmail.com>
2025-02-09 11:31:08 +0430

    fix(Crew): Fix the layout and navbar
    Fix the Layout and Page components styles and layout

--- app/crew/[member]/page.js
+++ app/crew/[member]/page.js
@@ -7,28 +7,22 @@ const CrewPage = async ({ params }) => {
   const { name, images, role, bio } = crew[member];
 
   return (
-    <section className="grid justify-center items-center mt-48 mb-20 md:mb-0 lg:grid-flow-row lg:grid-cols-2 ">
-      <div className=" flex flex-col items-center justify-center md:order-3 lg:order-2">
+    <section className="flex flex-col lg:flex-row justify-center lg:justify-start lg:gap-20 lg:pl-32  items-center lg:items-start mb-20 md:mb-0">
+      <div className=" flex flex-col items-center justify-center w-full lg:w-1/3 md:order-3 md:mt-20 lg:mt-5 lg:order-2 ">
         <Image
-          className="w-48 h-64 md:h-[25rem] md:w-[18rem] object-contain object-bottom lg:h-[30rem] lg:w-[25rem]"
+          className="w-48 h-64 md:h-[30rem] md:w-[24rem] lg:h-33rem] object-contain object-bottom"
           src={images[member]}
           alt={name}
         />
         <hr className="w-4/5 h-0.5 bg-darkGray opacity-30 md:hidden" />
       </div>
 
-      <div className=" h-10 md:order-2  md:h-24 lg:hidden"></div>
-
-      <div className="flex flex-col items-center justify-center gap-2 lg:gap-4 font-barlow md:order-1 lg:items-start -mt-4">
-        <h2 className="text-darkGray text-lg uppercase font-semibold tracking-tight lg:text-2xl lg:font-medium">
+      <div className="flex flex-col items-center justify-center lg:items-start gap-2 lg:gap-4 mt-16 px-8 lg:pl-0 font-barlow md:order-1  max-w-md md:max-w-lg lg:max-w-xl lg:mt-44">
+        <h2 className="text-darkGray text-xl uppercase font-semibold tracking-tight lg:text-2xl">
           {role}
         </h2>
-        <h3 className="text-2xl uppercase font-semibold text-white lg:text-5xl lg:font-medium ">
-          {name}
-        </h3>
-        <p className="text-lightGray text-center max-w-xl lg:text-start lg:max-w-sm">
-          {bio}
-        </p>
+        <h3 className="text-3xl md:text-4xl lg:text-5xl uppercase font-semibold md:font-medium lg:font-semibold text-white">{name}</h3>
+        <p className="text-lightGray leading-7 text-center lg:text-start">{bio}</p>
       </div>
     </section>
   );
--- app/crew/layout.js
+++ app/crew/layout.js
@@ -4,30 +4,20 @@ import Link from "next/link";
 
 const CrewLayout = ({ children }) => {
   return (
-    <section className="grid grid-cols-2 grid-rows-2 px-10 lg:px-32 crew-bg-img bg-img-config">
-      <div className="grid grid-rows-3 grid-cols-1 gap-2 h-min col-start-1 col-end-3 row-start-1 row-end-4 text-white z-10 mt-32 lg:mt-40 ">
-        <h1 className="flex gap-2 text-xl font-medium font-barlow uppercase tracking-widest [word-spacing:5px] lg:row-start-1 lg:row-end-2">
-          <span className="text-darkGray">02</span> <span>Meet your crew</span>
-        </h1>
+    <section className="crew-bg-img bg-img-config pt-1">
+      <ul className="absolute top-[30rem] md:top-[28rem] lg:top-[37rem] left-0 lg:left-32 right-0 lg:right-full mx-auto w-min flex items-center content-center gap-4">
+        {Object.keys(crew).map((item) => (
+          <li key={item}>
+            <Link
+              href={`/crew/${item}`}
+              title={item}
+              className="w-3 h-3 md:w-2.5 md:h-2.5 rounded-full bg-white inline-block"
+            ></Link>
+          </li>
+        ))}
+      </ul>
 
-        <div className="h-40 md:h-28 lg:hidden"></div>
-
-        <ul className="flex flex-row items-center content-center gap-4  h-max mx-auto lg:mx-0 lg:row-start-3 lg:mt-28">
-          {Object.keys(crew).map((item) => (
-            <li key={item}>
-              <Link
-                href={`/crew/${item}`}
-                title={item}
-                className="w-2 h-2 rounded-full bg-white inline-block"
-              ></Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="col-start-1 col-end-3 row-start-1 row-end-4 ">
-        {children}
-      </div>
+      <div className="mt-48 md:mt-40 lg:mb-0">{children}</div>
     </section>
   );
 };
